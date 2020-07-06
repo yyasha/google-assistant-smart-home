7b76d2866337c65688310d7d12211686fce26fae
yyasha <50919655+yyasha@users.noreply.github.com>
2020-07-06 21:58:18 +0400

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ TOKENS_DIRECTORY = "/home/google_home/tokens"
 DEVICES_DIRECTORY = "/home/google_home/devices"
 
 # Uncomment to enable logging
-#LOG_FILE = "/var/log/google-home.log"
-#LOG_LEVEL = logging.DEBUG
-#LOG_FORMAT = "%(asctime)s %(remote_addr)s %(user)s %(message)s"
-#LOG_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+LOG_FILE = "/root/google-home/google-home.log"
+LOG_LEVEL = logging.DEBUG
+LOG_FORMAT = "%(asctime)s %(remote_addr)s %(user)s %(message)s"
+LOG_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
